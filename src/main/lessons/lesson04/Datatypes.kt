1b3dfb98d7b5508378d16594c43b89d4eb0f5163
bishop <bishopgarden05@gmail.com>
2026-09-13 16:36:13 +0300

    homework 4 Базовые типы данных

---
+++ src/main/lessons/lesson04/Datatypes.kt
@@ -0,0 +1,84 @@
+package org.example.lesson04
+
+val v1: Byte = 42
+val v2: Long = 98765432123456789L
+val v3: Float = 23.45f
+val v4: Double = 0.123456789
+val v5: String = "Kotlin & Java"
+
+//Неверный регистр для Boolean
+//val v6 = FALSE
+
+val v7: Char = 'c'
+val v8: Short = 500
+val v9: Long = 4294967296L
+val v10: Float = 18.0f
+val v11: Double = -0.001
+val v12: String = "OpenAI"
+val v13: String = "true"
+val v14: List<Int> = listOf(3, 14)
+val v15: Char = '9'
+val v16: Short = 2048
+val v17: Long = 10000000000L
+val v18: Set<String> = setOf("OpenAI", "Quantum Computing")
+val v19: Float = 5.75f
+
+//Неверные кавычки для Char
+//val v20 = `1.414`
+
+val v21: String = "Artificial Intelligence"
+val v22: Array<Any> = arrayOf('x', "A")
+val v23: String = "Android Studio"
+val v24: Char = '@'
+val v25: Short = 1024
+val v26: Long = 1234567890123L
+val v27: Float = 10.01f
+val v28: Double = -273.15
+val v29: String = "SpaceX"
+
+//Неверный регистр для Boolean
+//val v30 = FALSE
+
+val v31: Double = 0.007
+
+//Неверные кавычки для String, эмодзи займет больше 1 символа, чтоб можно было обернуть в Char, да и для Char кавычки неверные
+//val v32 = “🤯”
+
+val v33: Map<String, Int> = mapOf("true" to 2, "false" to 34)
+
+//Неверные кавычки для String
+//val v34 = ‘65535’
+
+val v35: Long = 72057594037927935L
+val v36: Float = 2.71828f
+val v37: Double = 101.0101
+val v38: String = "Quantum Computing"
+val v39: Map<Int, String> = mapOf(2 to "true", 34 to "false")
+val v40: Char = 'x'
+val v41: Short = 314
+val v42: Long = 123456789123456789L
+
+val v43: Float = 6.626f
+
+//Неверный регистр для Boolean
+//val v44 = TRUE
+
+
+var mediumChoice: Char = 'a' //Хранить букву, на которую указывает палец медиума во время спиритического сеанса.
+val sugarSpoon: Double = 1.5//Хранить количество ложек сахара, которые я кладу в одну чашку чая.
+var listOfExpenses: Map<String, Int> = mapOf(
+    "Vkusvill" to 7500,
+    "Lavka" to 3000
+)//Хранить список расходов на доставку еды, чтобы ещё раз убедиться, что готовить было дешевле.
+var queueLengthAtStolovka: Double = 0.000000001//Хранить длину очереди в столовой до миллиардной доли сантиметра.
+var isBugClosed: Boolean = false//Хранить факт, закрыт ли баг после того, как его просто переименовали в фичу.
+var kettleWhistling: Int = 0//Хранить количество свистков чайника за день.
+var catPawToMyKeyboard: Int = 100500//Хранить количество нажатий котом на клавиатуру ноутбука за всё время твоей работы.
+var countingTheStars: Long = 98765432123456789L//Хранить количество попыток пересчитать звёзды на небе за всю историю человечества.
+var mattressAirMass: Float = 56.346f//Хранить массу воздуха в спускающем матрасе после нападения кота в долях грамма.
+var listOfLosers: Map<String, Long> = mapOf(
+    "Quibi" to 9_147_483_647L,
+    "FTX" to 12_147_483_647L
+)//Хранить словарь «название стартапа → сумму потерь инвесторов».
+val luckyBug: String = "починилось само"//Хранить строку «починилось само», чтобы закрывать тикеты без лишних слов.
+var meetingList: List<String> = listOf("как ускорить процессы", "внедряем ИИ")//Хранить список тем для митингов, которые на самом деле никому не нужны.
